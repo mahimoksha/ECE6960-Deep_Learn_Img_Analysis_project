e64c0844c2405ac846d679cbc429b175166a3167
mahimoksha <kmokshagna999@gmail.com>
2023-05-08 11:39:30 -0700

    added new models

---
+++ FasterRCNN/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e 
+set -x
+python3 -m pip install torch torchvision torchaudio
+python3 -m pip install pandas
+python3 -m pip install numpy
+python3 -m pip install matplotlib
+python3 -m pip install torchmetrics
+python3 -m pip install opencv-python
+python3 -m pip install PIL
+python3 -m pip install tqdm
